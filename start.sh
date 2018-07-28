ff58f41337c868dfdab5bee8f90f2df61b69ea37
cupcake <jbw@users.noreply.github.com>
2018-07-28 18:16:27 +0100

    add start script

--- start.sh
+++ start.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+docker rm -f hadoop-master
 
+docker run -itd --net=hadoop -p 50070:50070 -p 8088:8088 --name hadoop-master --hostname hadoop-master jbw/hadoop
+
+docker exec -it hadoop-master bash
